03ace9f792e66a574ceff158f6c32a6ced6795a7
Alexander Linder <alexander.linder@mondcarion.group>
2022-11-16 15:09:04 +0000

    Remove legacy systemd unit dump

--- build/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-pushd ./nixos-config
-git pull
-hosts=$(ls ./hosts)
-for host in $hosts
-do
-        nix --extra-experimental-features nix-command --extra-experimental-features flakes build .\#nixosConfigurations.$host.config.system.build.toplevel
-done
-popd
